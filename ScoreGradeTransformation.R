1baf1658d16700dfecd7537a432ad50814d58e37
Sharon Klinkenberg <s.klinkenberg@uva.nl>
2024-03-31 18:21:10 +0200

    Update ScoreGradeTransformation.R
    Added formula for guess correction on only closed questions

--- ScoreGradeTransformation.R
+++ ScoreGradeTransformation.R
@@ -1,5 +1,5 @@
-ScoreGradeTransformation <- function(score, max.score, guessing.score) {
-  # Transform raw scores to guessing corrected grades
+ScoreGradeTransformationCorrectForGuessingOnMax <- function(score, max.score, guessing.score) {
+  # Transform raw scores to guessing corrected for total amount of points on exam
   #
   # Args:
   #   score          : Total amount of raw points on an exam
@@ -15,5 +15,27 @@ ScoreGradeTransformation <- function(score, max.score, guessing.score) {
   grade[grade <  0] = 0
   grade[grade > 10] = 10
   
+  return(grade)
+}
+
+ScoreGradeTransformationCorrectForGuessingOnClosed <- function(closed.sum, open.sum, total.guess, max.closed, max.points) {
+  # Transform raw scores to guessing corrected for closed amount of points on exam
+  #
+  # Args:
+  #   closed.sum  : sum score integer for closed questions
+  #   open.sum    : sum score integer for open questions
+  #   total.guess : total amount of quessing points float
+  #   max.closed  : max amount of points possible for closed questions
+  #   max.points  : max amount of points possibla for entire exam
+  #  
+  # Returns:
+  #   grade : calculated grade as float Between 0 and 10
+  
+  grade = ( ( closed.sum - total.guess ) / ( max.closed - total.guess ) *  max.closed + open.sum ) / max.points * 10
+  
+  # Cap grade to minimum and maximum
+  grade[grade <  0] = 0
+  grade[grade > 10] = 10
+    
   return(grade)
 }
